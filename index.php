cd51c0fbda090a561d7423ab3df7e14cfc82196d
DraY <d-r-a-y@mail.ru>
2014-03-05 19:39:13 +0200

    add working copy

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: DraY
- * Date: 05.03.14
- * Time: 17:47
- */
 
-print 'hello world....';
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/../yii-1.1.14/framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
+
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
+Yii::createWebApplication($config)->run();
